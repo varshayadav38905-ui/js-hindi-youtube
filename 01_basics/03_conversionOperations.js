ae3fe83d44da9c0acc5bca72f11040a96d86efa0
varshayadav38905-ui <varshayadav38905@gmail.com>
2026-07-18 07:09:26 +0000

    conversion0perations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -31,3 +31,43 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// ************operations *******************
+let value = 3 
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2); power*/
+
+let str1 = "hello"
+let str2 = " varsha"
+
+let str3 = str1 + str2
+console.log(str3);
+
+/*console.log("1" + 2);
+console.log(1 +"2");
+console.log(1+2 +"2" );
+console.log("1"+2 +2 );
+
+/* if the first one is string ,then the rest of the numbers are treated as string too 
+and if the last one is string then the first numbers are treated as numbers 
+*/ 
+//console.log(5 - 6 * 5 / 9); // prefer not to write code like this ,this looks messy
+//console.log(true);
+//console.log(+true); // this will give us "1"
+//console.log(+"");  //  this will give 0
+
+let num1, num2, num3,
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
