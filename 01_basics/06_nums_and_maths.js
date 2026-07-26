197429788e9e707bfa605218c9aa6119df94bc10
varshayadav38905-ui <varshayadav38905@gmail.com>
2026-07-26 15:36:22 +0000

    maths and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,43 @@
+const score = 440   //it got automatically declared 
+/*console.log(score);
+console.log(typeof score);*/
+
+const balance = new Number(100) // JavaScript me new keyword lagte hi constructor ek object return karta hai.
+/*console.log(balance);
+
+console.log(balance. toString().length);
+console.log(typeof balance);        // javascript me at the end hr ek cheej object hi hoti h
+console.log(balance. toFixed(2));   //expected output = 100.00
+ 
+const otherNumber = 23.56689
+console.log(otherNumber.toPrecision(3)); // if 23.5467 = 23.5,,,if123.89 = 124,, if 1123.34 =1.12e+3
+// priority is given before decimal
+
+
+const hunderds = 1000000
+console.log(hunderds.toLocaleString('en-IN'));
+
+/******************MATHS**************************/
+console.log(Math);
+console.log(Math.abs(-4));  // negative value will be converted into positive
+console.log(Math.round(4.6)); //rounding off
+console.log(Math.ceil(4.2)); //upper value
+console.log(Math.floor(4.9)); //lower value
+console.log(Math.min(2,3,4,5));
+console.log(Math.max(2,3,4,5,6));
+
+console.log(Math.random());  //value will always netween zero and one
+console.log(Math.floor(Math.random()*10) + 1); 
+
+const min = 10;
+const max = 20;
+
+ console.log(Math.floor(Math.random() * (max - min + 1) + min));
+ 
+
+
+
+
+
+
+
