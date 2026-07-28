be4248b9e96e5f6effd95446af65353e5c23ad5c
varshayadav38905-ui <varshayadav38905@gmail.com>
2026-07-28 07:46:40 +0000

    obj

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,49 @@
+const tinderUser = new Object()  //singeton object
+const tinderUser ={}             //non singleton object
+tinderUser.id = "12345kdjf"
+tinderUser.name= "varsha"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullName :{
+        userfullname:{
+            firstname: "abc" ,
+            lastname : "xyz"
+        }
+    }
+}
+console.log(regularUser.fullName?.userfullname.firstname);
+
+
+const obj1 = { 1: "a" , 2 : " b"}
+const obj2 = {3: "d" , 4: "f"}
+
+// const obj3 = { obj1 , obj2}
+//const obj3 = Object.assign({}, obj1 , obj2 )
+const obj3 = {...obj1, ...obj2 }  // spread
+console.log(obj3);  // object k andr object aa jayeg
+
+
+const users = [
+    {
+        id: 1,
+        email : "v2234@gmail.com"
+    },
+    {
+    },
+    {
+    },
+]
+ users[1].email  //we can access the values from the database
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));  // Interesting
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
