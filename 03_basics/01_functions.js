f9763f555ff69ea987b6484eaf00747e65a0889e
varshayadav38905-ui <varshayadav38905@gmail.com>
2026-07-31 03:53:12 +0000

    objects and array in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,7 +14,7 @@ myWord()     // calling the function */
   // console.log(num1 +num2);
 //}
 
-function addTwoNumbers(num1 , num2){
+/*function addTwoNumbers(num1 , num2){
 
     let result = num1 + num2
     return result 
@@ -32,7 +32,47 @@ function loginusermessage(username){
   }
   return `${username} just logged in`
 }
-console.log(loginusermessage()); //if we dont pass anything it will show undefined
+console.log(loginusermessage()); */ //if we dont pass anything it will show undefined
 
 
+  function calculateCartValue(...num1){  // without ... only one value will be printed
+    return num1                          // rest operator 
+  }
+
+  console.log(calculateCartValue(200, 400, 500));
   
+//another case
+   function calculateCartValue(val1, val2, ...num1){ 
+    return num1                          
+  }
+
+  console.log(calculateCartValue(200, 400, 500)); // expected output = [ 500 ]
+
+
+  // object in function
+  const user = {
+    username : "varsha" ,
+    price: 199
+  }
+  function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+  
+  }
+  //handleObject(user) 
+
+  /*   another way  */
+  handleObject({
+    username : "sam" ,
+    price : 399
+  })     
+
+
+  // arrays in function
+
+  const myNewArray = [200, 400, 500 ,700]
+
+  function returnValue(anyArray){
+    return anyArray[2]
+  }
+ // console.log(returnValue(myNewArray));
+ console.log(returnValue([200, 400 ,500 , 600])); // we can directly pass array here like object
