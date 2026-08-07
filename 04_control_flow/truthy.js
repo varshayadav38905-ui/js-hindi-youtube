fd7d893af3907a32eafe3f52d44f19530b05abab
varshayadav38905-ui <varshayadav38905@gmail.com>
2026-08-07 04:24:47 +0000

    control flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -34,6 +34,6 @@ console.log(val2);
 
 condition ? true : false
 const iceTeaPrice = 100
-iceTeaPrice >=80 ? console.log("less than 80") : console.log("more than 80");
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
 
 
