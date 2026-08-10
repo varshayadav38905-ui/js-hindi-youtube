cc79f01b29734959e545c8a7a77e3638ab5c359e
varshayadav38905-ui <varshayadav38905@gmail.com>
2026-08-10 17:43:03 +0000

    for of

---
+++ 05_iterations/03_loops.js
@@ -0,0 +1,13 @@
+
+const myArray = ["varsha", "yadav", "annu"]
+
+for (const names of myArray) {
+    console.log(names);
+    
+}
+
+const name = "varsha yadav"
+for (const letters of name) {
+    console.log(letters);
+    
+}
